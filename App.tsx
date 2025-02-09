87ca67ca1dba40e97b5eca1047557ea42f68b02e
Adriano <adrianoact@msn.com>
2025-02-09 18:39:03 -0300

    background notification click

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { OneSignal } from 'react-native-onesignal';
+import { NotificationClickEvent, OneSignal } from 'react-native-onesignal';
 
 import { Platform, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
@@ -11,6 +11,7 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 import { tagUserInfoCreate } from './src/notifications/notificantionsTags';
+import { useEffect } from 'react';
 
 const oneSignalAppId = Platform.OS === 'ios' ? 'iOS-ID' : '3626b1dd-242f-475a-9234-32424628b09a';
 OneSignal.initialize(oneSignalAppId);
@@ -20,6 +21,18 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate();  
+
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent): void => {
+      console.log('Notificação aberta');
+    }
+    
+    OneSignal.Notifications.addEventListener("click", handleNotificationClick)
+
+    return () => {
+      OneSignal.Notifications.removeEventListener("click", handleNotificationClick)
+    }
+  },[]);
 
   return (
     <NativeBaseProvider theme={THEME}>
